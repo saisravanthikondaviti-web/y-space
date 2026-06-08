35783f4c1952245bde6bac5766936352605307a3
Sai-Sravanthi-Kondaviti <sravanthikondaviti16@gmail.com>
2026-06-08 10:36:41 +0530

    change of herobackground

--- src/components/home/HeroBackground.tsx
+++ src/components/home/HeroBackground.tsx
@@ -1,68 +1,31 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { useMemo } from "react";
+import { useEffect, useState } from "react";
+
+type Particle = {
+  left: string;
+  top: string;
+  duration: number;
+};
 
 export default function HeroBackground() {
-  const particles = useMemo(
-    () =>
+  const [particles, setParticles] = useState<Particle[]>([]);
+
+  useEffect(() => {
+    setParticles(
       Array.from({ length: 20 }, () => ({
         left: `${Math.random() * 100}%`,
         top: `${Math.random() * 100}%`,
         duration: 3 + Math.random() * 5,
-      })),
-    []
-  );
+      }))
+    );
+  }, []);
 
   return (
     <>
-      {/* Indigo Orb */}
-      <motion.div
-        animate={{
-          x: [0, 80, 0],
-          y: [0, -50, 0],
-        }}
-        transition={{
-          duration: 15,
-          repeat: Infinity,
-          ease: "easeInOut",
-        }}
-        className="
-          absolute
-          left-[20%]
-          top-[30%]
-          h-[500px]
-          w-[500px]
-          rounded-full
-          bg-[#616CFA]/20
-          blur-[140px]
-        "
-      />
+      {/* Orbs */}
 
-      {/* Orchid Orb */}
-      <motion.div
-        animate={{
-          x: [0, -80, 0],
-          y: [0, 60, 0],
-        }}
-        transition={{
-          duration: 18,
-          repeat: Infinity,
-          ease: "easeInOut",
-        }}
-        className="
-          absolute
-          right-[10%]
-          top-[20%]
-          h-[400px]
-          w-[400px]
-          rounded-full
-          bg-[#E46ECC]/20
-          blur-[120px]
-        "
-      />
-
-      {/* Floating Particles */}
       {particles.map((particle, i) => (
         <motion.div
           key={i}
@@ -75,13 +38,7 @@ export default function HeroBackground() {
             repeat: Infinity,
             ease: "easeInOut",
           }}
-          className="
-            absolute
-            h-1
-            w-1
-            rounded-full
-            bg-white
-          "
+          className="absolute h-1 w-1 rounded-full bg-white"
           style={{
             left: particle.left,
             top: particle.top,
