72714fed0671293f9d635d7a3b01b2fef6841de5
Sai-Sravanthi-Kondaviti <sravanthikondaviti16@gmail.com>
2026-07-28 23:20:32 +0530

    fix next config for dynamic deployment

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: false,
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**",
-      },
-      {
-        protocol: "http",
         hostname: "**",
       },
     ],
